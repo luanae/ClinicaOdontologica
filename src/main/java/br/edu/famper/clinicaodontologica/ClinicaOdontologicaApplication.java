0c6b3f4b746ca314963a538cd4f0a2b0d4784f4f
Luana Pereira <luanaelizapereira@gmail.com>
2024-09-25 20:02:37 -0300

    Initial commit

---
+++ src/main/java/br/edu/famper/clinicaodontologica/ClinicaOdontologicaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.famper.clinicaodontologica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaOdontologicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaOdontologicaApplication.class, args);
+    }
+
+}
